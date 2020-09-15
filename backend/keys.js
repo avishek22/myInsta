de67658f45de1b1a06d7414bba86acc4dbf3a39f
Avishek <avishekbhattacharjee35@gmail.com>
2020-09-15 23:02:26 +0530

    keys

---
+++ backend/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://avishek:stitches22@cluster0.oo78r.mongodb.net/Instagram?retryWrites=true&w=majority",
+
+  JWT_SECRET: "uiwahldhwahlaospfclm",
+};
